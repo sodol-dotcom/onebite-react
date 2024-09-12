49970bee7aeca339e8d3ee47b3ef6482da1c4b8f
sodol-dotcom <sojeong0521@gmail.com>
2024-09-12 20:42:20 +0900

    단락 평가 오타 수정

--- section02/chapter02.js
+++ section02/chapter02.js
@@ -1,15 +1,23 @@
-// 단락 평가
-function returnFalse() {
-    console.log("False 함수");
-    return false;
-}
+// // 단락 평가
+// function returnFalse() {
+//     console.log("False 함수");
+//     return false;
+// }
 
-function returnTrue() {
-    console.log("Trut 함수");
-    return true;
-}
+// function returnTrue() {
+//     console.log("Trut 함수");
+//     return true;
+// }
 
-console.log(returnFalse() && returnTrue());   
-// 'returnFalse' 함수의 콘솔로그 까지만 찍히고 false가 찍힌다.
-// 'returnTrue'의 콘솔로그는 찍히지 않음 
-// => 단락평가가 적용되어서 그런 것. 'returnTrue()' 함수는 호출조차 되지 않음
+// console.log(returnFalse() && returnTrue());
+// // 'returnFalse' 함수의 콘솔로그 까지만 찍히고 false가 찍힌다.
+// // 'returnTrue'의 콘솔로그는 찍히지 않음
+// // => 단락평가가 적용되어서 그런 것. 'returnTrue()' 함수는 호출조차 되지 않음
+
+// 단락 평가 활용 사례
+function printName(person) {
+  const name = person && person.name;
+  console.log(name || "person의 값이 없음");
+}
+printName();
+printName({ name: "이정환" });
