8bda3318a978dffe7ca0194f4839171b98b472ee
sodol-dotcom <sojeong0521@gmail.com>
2024-10-02 16:52:53 +0900

    Node.js 모듈 시스템 이해하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,8 @@
-console.log("안녕 Node.js");
+//const {add, sub} = require("./math"); // CommonJS 사용
+
+// ESmodule 사용
+// 확장자명 꼭 붙여줘야함(math.js)
+import { add, sub } from "./math.js";
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
---
+++ section03/src/math.js
@@ -0,0 +1,30 @@
+// math 모듈
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+// // CommonJS 이용
+// module.exports = {
+//   add,
+//   sub,
+// };
+
+
+function multiply(a, b) {
+  return a * b;
+}
+export { multiply };
+
+
+
+export function multiply(a, b) {
+  return a * b;
+}
+
+
+
