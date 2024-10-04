bc3c31b429d25b536d788471ff1183d66784fa7a
sodol-dotcom <sojeong0521@gmail.com>
2024-10-04 12:20:35 +0900

    5.2 React 컴포넌트

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
-    <h1>안녕 리액트!</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,10 @@
+// 컴포넌트명은 꼭 대문자로 시작
+const Header = () => {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+};
+
+export default Header;
