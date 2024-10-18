88452aa5fe1a57cdd7fc6f22f1046cc92b34fe8b
sodol-dotcom <sojeong0521@gmail.com>
2024-10-18 18:42:16 +0900

    7.3 useEffect로 라이프사이클 제어하기

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,17 +1,30 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import Even from "./components/Even";
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
+  const isMount = useRef(false);
 
+  // 1. 마운트: 탄생 -> []를 빈 배열로 설정하면 됨.
   useEffect(() => {
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]);
-  // 의존성 배열
-  // dependency array = deps
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트: 변화, 리렌더링 -> [] 생략하면 됨.
+  useEffect(() => {
+    if (!isMount.current) {
+      // 레퍼런스 객체를 플래그로 사용해서 업데이트 되었을 때만 update가 콘솔창에 찍히도록 함
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트: 죽음
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -30,6 +43,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
