cb766ad187d21d5efa86674bf476470e6b867e36
sodol-dotcom <sojeong0521@gmail.com>
2024-11-05 11:55:21 +0900

    8.6 Update-투두 수정하기(2)

--- section08(2)/src/App.jsx
+++ section08(2)/src/App.jsx
@@ -7,19 +7,19 @@ import List from "./components/List";
 const mockDate = [
   {
     id: 0,
-    isdDone: false,
+    isDone: false,
     content: "React 공부하기",
     date: new Date().getTime(),
   },
   {
     id: 1,
-    isdDone: false,
+    isDone: false,
     content: "빨래하기",
     date: new Date().getTime(),
   },
   {
     id: 2,
-    isdDone: false,
+    isDone: false,
     content: "노래 연습하기",
     date: new Date().getTime(),
   },
@@ -38,11 +38,21 @@ function App() {
 
     setTodos([newTodo, ...todos]);
   };
+
+  const onUpdate = (targetId) => {
+    // todos State 값들 중에 targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
+    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
+    setTodos(
+      todos.map((todo) =>
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
   return (
     <div className="App">
       <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} />
+      <List todos={todos} onUpdate={onUpdate} />
     </div>
   );
 }
