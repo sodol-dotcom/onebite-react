e1c5f81fd245beb21943a151ba7d3b4b62ba0fd2
sodol-dotcom <sojeong0521@gmail.com>
2024-11-08 12:24:22 +0900

    12.3 페이지 라우팅 (2)라우팅 설정하기

---
+++ section12/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Nonfound from "./pages/Notfound";
+
+// 1. "/": 모든 일기를 조회하는 Home 페이지
+// 2. "/new": 새로운 일기를 작성하는 New 페이지
+// 3. "diary": 일기를 상세히 조회하는 Diary 페이지
+const App = () => {
+  return (
+    // Routes 안에는 Route 컴포넌만 들어갈 수 있고
+    // Routes 밖에 배치된 요소들은 페이지 라우팅과 관련없이 모든 페이지에 동일하게 렌더링 된다.(Notfound에서도!)
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<Nonfound />} />
+    </Routes>
+  );
+};
+export default App;
---
+++ section12/src/pages/Notfound.jsx
@@ -0,0 +1,5 @@
+const Nonfound = () => {
+  return <div>잘못된 페이지입니다.</div>;
+};
+
+export default Nonfound;
