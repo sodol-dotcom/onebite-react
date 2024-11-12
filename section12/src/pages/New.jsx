c0b6cbb3447a14e5a7b7d8df07294b780b340d0f
sodol-dotcom <sojeong0521@gmail.com>
2024-11-12 18:13:44 +0900

    12.13 [감정일기장 프로젝트] New 페이지 구현하기(1) - UI

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,4 +1,13 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div>
+      <Header title={"일기 쓰기"} leftChild={<Button text={"🡠 뒤로 가기"} />} />
+      <Editor />
+    </div>
+  );
 };
 export default New;
